c5e7cbb246676be11c316beabe60ce7152836642
Billy Zhou <billyzh@amazon.com>
2023-03-15 14:12:53 -0400

    bump version

--- container-insights-manifest-update.sh
+++ container-insights-manifest-update.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 k8sDirPrefix="./k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring"
 ecsDirPrefix="./ecs-task-definition-templates/deployment-mode/daemon-service/cwagent-ecs-instance-metric"
 
-newK8sVersion="k8s/1.3.12"
-agentVersion="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:1.247357.0b252275"
+newK8sVersion="k8s/1.3.13"
+agentVersion="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:1.247358.0b252413"
 fluentdVersion="fluent/fluentd-kubernetes-daemonset:v1.7.3-debian-cloudwatch-1.0"
 fluentBitVersion="public.ecr.aws/aws-observability/aws-for-fluent-bit:stable"
 
