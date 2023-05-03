0e3a868054766d9b91ca8ea99b02d76748a0ea50
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2023-05-03 10:29:28 -0400

    release agent version 1.247359.0b252558 (#132)

--- container-insights-manifest-update.sh
+++ container-insights-manifest-update.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 k8sDirPrefix="./k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring"
 ecsDirPrefix="./ecs-task-definition-templates/deployment-mode/daemon-service/cwagent-ecs-instance-metric"
 
-newK8sVersion="k8s/1.3.13"
-agentVersion="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:1.247358.0b252413"
+newK8sVersion="k8s/1.3.14"
+agentVersion="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:1.247359.0b252558"
 fluentdVersion="fluent/fluentd-kubernetes-daemonset:v1.7.3-debian-cloudwatch-1.0"
 fluentBitVersion="public.ecr.aws/aws-observability/aws-for-fluent-bit:stable"
 
