1d05e96a9f541e966faab571dd947933157966fb
lisguo <lguo25@gmail.com>
2024-06-10 11:19:05 -0400

    Release k8s/1.3.24

--- container-insights-manifest-update.sh
+++ container-insights-manifest-update.sh
@@ -5,8 +5,8 @@ k8sDirPrefix="./k8s-deployment-manifest-templates/deployment-mode/daemonset/cont
 k8sQSDirPrefix="./k8s-quickstart"
 ecsDirPrefix="./ecs-task-definition-templates/deployment-mode/daemon-service/cwagent-ecs-instance-metric"
 
-newK8sVersion="k8s/1.3.23"
-agentVersion="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:1.300039.0b612"
+newK8sVersion="k8s/1.3.24"
+agentVersion="public.ecr.aws/cloudwatch-agent/cloudwatch-agent:1.300040.0"
 fluentdVersion="fluent/fluentd-kubernetes-daemonset:v1.10.3-debian-cloudwatch-1.0"
 fluentBitVersion="public.ecr.aws/aws-observability/aws-for-fluent-bit:stable"
 fluentBitWindowsVersion="public.ecr.aws/aws-observability/aws-for-fluent-bit:windowsservercore-stable"
